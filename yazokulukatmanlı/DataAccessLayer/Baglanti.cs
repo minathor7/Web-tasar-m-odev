9e64b402a2b74d677c66829d615527d612bfc5ea
minathor7 <mert0732900@gmail.com>
2021-06-10 00:42:49 +0300

    Add files via upload

---
+++ yazokulukatmanlı/DataAccessLayer/Baglanti.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataAccessLayer
+{
+    public class Baglanti
+    {
+        public static SqlConnection bgl = new SqlConnection(@"Data Source=DESKTOP-J9V0010;Initial Catalog=Dbo_yazokulu;Integrated Security=True");
+
+    }
+}
